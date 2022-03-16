4a8e3ccf633bc8a8266f707b5b0f4c00b85cca2d
SergioGarciaGil <sergio.cuerda@gmail.com>
2022-03-15 21:31:06 -0500

    homework

--- 03-sequelize/homework/routes/categories.js
+++ 03-sequelize/homework/routes/categories.js
@@ -1,17 +1,22 @@
-const { Page, Category } = require('../models');
-const router = require('express').Router();
+const { Page, Category } = require("../models");
+const router = require("express").Router();
 
-router.get('/', function(req, res) {
+router.get("/", async function (req, res) {
   // Modificar para devolver los datos de todas las categorias existentes
   // Tu código acá:
-
+  const categories = await Category.findAll();
+  res.json(categories);
 });
 
-router.get('/:idCategory', function(req, res) {
+router.get("/:idCategory", function (req, res) {
   // Modificar para que cuando se seleccione una "Category" en particular se muestren
   // todas las páginas que estén dentro de dicha categoría
   // Tu código acá:
-
+  const { id } = req.params;
+  const category = Category.findByPk(id, {
+    include: Pages,
+  });
+  res.json(category);
 });
 
 module.exports = router;
--- 03-sequelize/homework/routes/pages.js
+++ 03-sequelize/homework/routes/pages.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const { Page, User } = require("../models");
 
-router.post("/", function (req, res) {
+router.post("/", async function (req, res) {
   // Modificar para que cuando se clickee el botón de "SUBMIT" se cree un nuevo post
   // tomando los datos desde el form y agregándolo a la base de datos
   // (Debe incluir también la categoría a la/s cual/es pertenece)
@@ -28,7 +28,7 @@ router.get("/add", function (req, res) {
   res.render("addpage");
 });
 
-router.get("/:urlTitle", function (req, res) {
+router.get("/:urlTitle", async function (req, res) {
   // Modificar para que cuando se seleccione un "Page" en particular se muestren
   // los datos asociados al mismo
   // Tu código acá:
